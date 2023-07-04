238f407a9e210cc75e157a82aa5f39ddabd27acd
Brent Simmons <brents@patriot1tech.com>
2023-07-04 13:38:25 -0300

    Updating base path and background color

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { store } from './store';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ulty-lineup">
       <Provider store={store}>
         <App />
       </Provider>
