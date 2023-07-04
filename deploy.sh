d34e078dac81018d4f9d1229dee2b0ddb178c2a4
Brent Simmons <brents@patriot1tech.com>
2023-07-04 13:11:42 -0300

    Updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,15 @@ npm i
 npm run build
 git checkout deploy
 
+# clean up old build
+rm index.html
+rm -rf assets
+rm sports*
+rm vite*
+
+# move dist build
+mv dist/* ./
+
 git add .
-git commit -m "Deploying..."
-git push
+# git commit -m "Deploying..."
+# git push
