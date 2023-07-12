221a875cc1cbdc4f29bf20dcbfb2331e37a2cf02
Brent Simmons <brents@patriot1tech.com>
2023-07-11 22:56:04 -0300

    Adding dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,43 @@ import { Routes, Route, Outlet, Link } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import PlayerRoster from "./PlayerRoster";
 import PlayerLineup from "./PlayerLineup";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function App() {
   return (
-    <div>
-      {/* Routes nest inside one another. Nested route paths build upon
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div>
+        {/* Routes nest inside one another. Nested route paths build upon
             parent route paths, and nested route elements render inside
             parent route elements. See the note about <Outlet> below. */}
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<LandingPage />} />
-          <Route path="roster" element={<PlayerRoster />} />
-          <Route path="lineup" element={<PlayerLineup />} />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<LandingPage />} />
+            <Route path="roster" element={<PlayerRoster />} />
+            <Route path="lineup" element={<PlayerLineup />} />
 
-          {/* Using path="*"" means "match anything", so this route
+            {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
-    </div>
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
 function Layout() {
   return (
     <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/roster">Roster</Link>
-          </li>
-          <li>
-            <Link to="/lineup">Line Up</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
       <Outlet />
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
@@ -10,10 +10,10 @@ import { store } from './store';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ulty-lineup">
+    <HashRouter basename="/">
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
